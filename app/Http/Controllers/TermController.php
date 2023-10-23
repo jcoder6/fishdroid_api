3bca11bd8881210f7837a613bf5607f740b89c4a
Jomer Dorego <jdorego06@gmial.com>
2023-10-23 18:32:20 +0800

    add the api implementation for term just read all term and show 1 by id

---
+++ app/Http/Controllers/TermController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Term;
+use Illuminate\Http\Request;
+
+class TermController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        return Term::all();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Term::find($id);
+    }
+}
